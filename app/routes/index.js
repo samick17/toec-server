e6e5a97471eaf3edeb092e02bdb547c2b9c3f3a1
samick17 <boneache@gmail.com>
2019-05-04 22:52:45 +0800

    1. update index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/version', function(req, res, next) {
   res.json({
-    version: '1.0.1'
+    version: '1.0.0'
   });
 });
 
