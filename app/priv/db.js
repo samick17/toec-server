52731c6f2612e579f0043d099ebd7498511d9133
samick17 <boneache@gmail.com>
2019-05-04 15:22:18 +0800

    1. add db module 2. implement db init method.

---
+++ app/priv/db.js
@@ -0,0 +1,44 @@
+const DB = require('toec-db');
+
+async function init() {
+	let processEnv = process.env;
+	let env = {
+		TOEC_SECRET: processEnv.TOEC_SECRET
+	};
+	let missingArgs = [];
+	if(!env.TOEC_SECRET) {
+		missingArgs.push('TOEC_SECRET');
+	}
+	let dbOptionsDefs = [{
+		key: 'id',
+		envKey: 'TOEC_DBUSER_ID'
+	},{
+		key: 'pw',
+		envKey: 'TOEC_DBUSER_PW'
+	},{
+		key: 'host',
+		envKey: 'TOEC_DB_HOST'
+	},{
+		key: 'port',
+		envKey: 'TOEC_DB_PORT'
+	},{
+		key: 'db',
+		envKey: 'TOEC_DB_NAME'
+	}];
+	let dbOptions = dbOptionsDefs.reduce((m, i) => {
+		let value = processEnv[i.envKey];
+		if(!value) {
+			missingArgs.push(i.envKey);
+		}
+		m[i.key] = value;
+		return m;
+	}, {});
+	if(missingArgs.length) {
+		throw new Error(`The following arguments is not defined in process.env:\n${missingArgs.join('\n')}\nPlease define and try again.`);
+	}
+	await DB.init(env, dbOptions);
+}
+
+module.exports = {
+	init: init
+};
