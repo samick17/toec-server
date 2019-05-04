5144d7eef6c2fe236efa1750c89c3527c755d3af
samick <boneache@gmail.com>
2019-05-04 20:59:16 +0800

    1. update deploy scripts

--- ci/deploy.js
+++ ci/deploy.js
@@ -1,8 +1,23 @@
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-let appDir = path.join(__dirname, '../app');
-let child = spawn('docker-compose', ['up', '-d', '--build'], {
-	stdio: 'inherit',
-	cwd: appDir
-});
+function asyncSpawn(cwd, cmd, args) {
+	return new Promise(resolve => {
+		let proc = spawn(cmd, args, {
+			stdio: 'inherit',
+			cwd: cwd
+		});
+		proc.on('close', () => {
+			resolve();
+		});
+	});
+}
+
+function buildDocker() {
+	let cwd = path.join(__dirname, '../app');
+	return asyncSpawn(cwd, 'docker-compose', ['up', '-d', '--build']);
+}
+
+(async () => {
+	await buildDocker();
+})();
