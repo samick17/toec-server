1881d84230baa77c89159ed129dd78e7fe267f6b
samick <boneache@gmail.com>
2019-05-04 17:40:40 +0800

    1. extract server-port/host name

--- app/env.js
+++ app/env.js
@@ -1,4 +1,4 @@
 module.exports = {
 	host: '127.0.0.1',
-	port: 54000
+	port: 53200
 };
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
 git pull
+export SERVER_PORT=53200
+export INTERNAL_SERVER_PORT=53200
+export HOST_NAME=host.docker.internal
+export HOST_IP=10.0.2.15
 node ./ci/deploy.js
